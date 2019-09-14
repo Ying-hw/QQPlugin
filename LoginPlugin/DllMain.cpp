fb04617991602186d8e8a5bb56314f766a4d1696
Ying <1186488719@qq.com>
2019-09-14 18:30:39 +0800

    更新框架

--- LoginPlugin/DllMain.cpp
+++ LoginPlugin/DllMain.cpp
@@ -2,6 +2,7 @@
 #include "MainWidget.h"
 #include "Login.h"
 
-extern "C" LOGINPLUGIN_EXPORT MainWidget* Handle(void *data) {
-	return new Login();
+extern "C" LOGINPLUGIN_EXPORT void Handle() {
+	Login* login = new Login();
+	login->show();
 }
--- LoginPlugin/Login.cpp
+++ LoginPlugin/Login.cpp
@@ -5,9 +5,11 @@ Login::Login(QWidget *parent)
 	: MainWidget(parent)
 {
 	ui.setupUi(this);
+	//setGeometry(500, 300, 1800, 800);
+	//updateGeometry()
 }
 
 Login::~Login()
 {
-
+	
 }
